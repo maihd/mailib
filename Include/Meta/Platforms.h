0edd255195162bc73c298f1b6ed9040c44d01bf6
maihd <maihd.dev@gmail.com>
2022-09-23 11:13:30 +0700

    Refactor Misc/Platform.h

--- Include/Meta/Platforms.h
+++ Include/Meta/Platforms.h
@@ -1,9 +1,9 @@
 #pragma once
 
-
 #if !defined(_WIN32) && !defined(__linux__) && !defined(__darwin__)
 #define PLATFORM_BAREBONE
 #else
 #define PLATFORM_HOSTED
 #endif
 
+//! LEAVE AN EMPTY LINE HERE, REQUIRE BY GCC/G++
